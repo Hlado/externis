41bdb81e7fde11ca71ce761cba80b799bb35e729
Ivan Romanov <iwromanov@gmail.com>
2026-02-01 20:00:39 +0000

    Refactored pp profiler

--- src/pp-profiler.cpp
+++ src/pp-profiler.cpp
@@ -6,11 +6,8 @@
 
 #include <array>
 #include <cassert>
-#include <cstring>
 #include <memory>
-#include <stack>
 #include <string>
-#include <type_traits>
 #include <unordered_map>
 #include <variant>
 
@@ -43,64 +40,77 @@ class HeadersTracker
 public:
   explicit HeadersTracker(std::shared_ptr<Trace> trace)
     : mTrace{std::move(trace)}
+    , initialDepth{mTrace->depth()}
   {
 
   }
 
   void handleFileChange(cpp_reader *reader, const line_map_ordinary *lineMap)
   {
-    static constexpr auto  UNNAMED = "<unnamed>";
-    //It seems map is null in the end when preprocessor returns to main file
-    if(lineMap != nullptr) {
-      if(lineMap->reason == LC_ENTER) {
-        auto fileNameRaw = ORDINARY_MAP_FILE_NAME(lineMap);
-        auto fileName = std::string{UNNAMED};
-        
-        if(fileNameRaw != nullptr) {
-          fileName = fileNameRaw;
-          auto it = mHeadersVisits.find(fileName);
-          if(it == mHeadersVisits.end()) {
-            mHeadersVisits[fileName] = 1;
-          } else {
-            //Once visit number hit zero it becomes unchangeable (or at least that's the idea)
-            if(it->second > 0) {
-              it->second += 1;
-            }
-          }
-        }
+    if(lineMap == nullptr) {
+      return;
+    }
 
-        mTrace->push(fileName);
-      } else if(lineMap->reason == LC_LEAVE) {
-        //We either need to track initial depth or part ways with this assertion
-        //assert(((void)"enter/leave file preprocessor callback mismatch", mTrace->depth() > 1 mStages.size() > 0));
-
-        auto fileName = mTrace->name();
-        if(fileName == UNNAMED) {
-          collapse(*mTrace, mTrace->depth() - 1);
-        } else {
-          auto it = mHeadersVisits.find(fileName);
-          assert(((void)"enter/leave file preprocessor callback mismatch", it != mHeadersVisits.end()));
-
-          if(it->second == 0) {
-            //We assume that every file included only once (effectively) and if we already measured it, we just skip repeated appearance
-            mTrace->drop();
-          } else if(it->second == 1) {
-            //This means it is first (and last) time we got chance to measure
-            collapse(*mTrace, mTrace->depth() - 1);
-            it->second = 0;
-          } else {
-            //This means we encounter recursive include and in assumption of include guards we just skip nested ones
-            mTrace->drop();
-            it->second -= 1;
-          }
-        }
-      }
+    if(lineMap->reason == LC_ENTER) {
+      handleEnter(lineMap);
+    } else if(lineMap->reason == LC_LEAVE) {
+      handleLeave(lineMap);
     }
   }
 
 private:
+  static constexpr auto UNNAMED_FILE_NAME = "<unnamed>";
+
   std::shared_ptr<Trace> mTrace;
   std::unordered_map<std::string, std::size_t> mHeadersVisits;
+  std::size_t initialDepth;
+
+  void handleEnter(const line_map_ordinary *lineMap)
+  {
+    auto rawName = ORDINARY_MAP_FILE_NAME(lineMap);
+    auto name = std::string{UNNAMED_FILE_NAME};
+
+    //Named files require bit of a processing
+    if(rawName != nullptr) {
+      name = rawName;
+      visit(name);
+    }
+
+    mTrace->push(name);
+  }
+
+  //We assume that every file has include guard so all occurences except first is instant and we skip them
+  void visit(const std::string &name)
+  {
+    auto [it, inserted] = mHeadersVisits.insert(std::make_pair(name, 1));
+    //We don't touch zeroes because it means that file was once processed already
+    if(!inserted && it->second > 0) {
+      it->second += 1;
+    }
+  }
+
+  void handleLeave(const line_map_ordinary *lineMap)
+  {
+    assert(mTrace->depth() > initialDepth);
+
+    auto name = mTrace->name();
+    if(name != UNNAMED_FILE_NAME) {
+      auto it = mHeadersVisits.find(name);
+      assert(it != mHeadersVisits.end());
+
+      if(it->second == 0) { //Skip already processed files
+        mTrace->drop();
+      } else if(it->second == 1) { //Leaving header on first occurence
+        collapse(*mTrace, mTrace->depth() - 1);
+        it->second = 0;
+      } else { //Recursive includes on first encounter, skipping nested
+        mTrace->drop();
+        it->second -= 1;
+      }
+    } else {
+      collapse(*mTrace, mTrace->depth() - 1);
+    }
+  }
 };
 
 class MacroTracker
@@ -167,27 +177,26 @@ public:
     if(mReader == nullptr) {
       throw Error{"reader is not set"};
     }
-    mReaderCallbacksAddress = cpp_get_callbacks(mReader);
-    if(mReaderCallbacksAddress == nullptr) {
+    mReaderCallbacks = cpp_get_callbacks(mReader);
+    if(mReaderCallbacks == nullptr) {
       throw Error{"preprocessor callbacks are empty"};
     }
-    mOriginalCallbacks = *mReaderCallbacksAddress;
+    mOriginalCallbacks = *mReaderCallbacks;
     mOurCallbacks = mOriginalCallbacks;
     mReadersMapping[mReader] = this;
 
     try
     {
-      mReaderCallbacksAddress->file_change = &dispatchCallback<&PpProfilerImpl::handleFileChange, cpp_reader *, const line_map_ordinary *>;
-      mOurCallbacks.file_change = mReaderCallbacksAddress->file_change;
+      #define INSIGHT_PPP_SET_CALLBACK(handler, callback, ...)                                                                                             \
+        mReaderCallbacks->callback = &dispatchCallback<&PpProfilerImpl::handleCallback<&PpProfilerImpl::handler, &cpp_callbacks::callback , __VA_ARGS__>>; \
+        mOurCallbacks.callback = mReaderCallbacks->callback;
 
-      mReaderCallbacksAddress->used = &dispatchCallback<&PpProfilerImpl::handleUsed, cpp_reader *, location_t, cpp_hashnode *>;
-      mOurCallbacks.used = mReaderCallbacksAddress->used;
+      INSIGHT_PPP_SET_CALLBACK(handleFileChange, file_change, cpp_reader *, const line_map_ordinary *);
+      INSIGHT_PPP_SET_CALLBACK(handleLineChange, line_change, cpp_reader *, const cpp_token *, int);
+      INSIGHT_PPP_SET_CALLBACK(handleUsed, used, cpp_reader *, location_t, cpp_hashnode *);
+      INSIGHT_PPP_SET_CALLBACK(handleUsedDefine, used_define, cpp_reader *, location_t, cpp_hashnode *);
 
-      mReaderCallbacksAddress->used_define = &dispatchCallback<&PpProfilerImpl::handleUsedDefine, cpp_reader *, location_t, cpp_hashnode *>;
-      mOurCallbacks.used_define = mReaderCallbacksAddress->used_define;
-      
-      mReaderCallbacksAddress->line_change = &dispatchCallback<&PpProfilerImpl::handleLineChange, cpp_reader *, const cpp_token *, int>;
-      mOurCallbacks.line_change = mReaderCallbacksAddress->line_change;
+      #undef INSIGHT_PPP_SET_CALLBACK
     }
     catch(const std::exception& e)
     {
@@ -208,12 +217,75 @@ private:
   static inline std::unordered_map<cpp_reader *, PpProfilerImpl *> mReadersMapping;
 
   const Options mOptions;
+  cpp_reader *mReader{nullptr};
+  cpp_callbacks *mReaderCallbacks{nullptr};
   cpp_callbacks mOriginalCallbacks;
   cpp_callbacks mOurCallbacks;
-  cpp_reader *mReader{nullptr};
-  cpp_callbacks *mReaderCallbacksAddress{nullptr};
   HeadersTracker mHeadersTracker;
   MacroTracker mMacroTracker;
+
+
+  void handleLineChange(cpp_reader *reader, const cpp_token *token, int line)
+  {
+    mMacroTracker.handleLineChange(reader, token, line);
+  }
+
+  void handleUsed(cpp_reader *reader, location_t loc, cpp_hashnode *node)
+  {
+    mMacroTracker.handleUsedKind(reader, loc, node);
+  }
+
+  void handleUsedDefine(cpp_reader *reader, location_t loc, cpp_hashnode *node)
+  {
+    mMacroTracker.handleUsedKind(reader, loc, node);
+  }
+
+  void handleFileChange(cpp_reader *reader, const line_map_ordinary *lineMap)
+  {
+    mHeadersTracker.handleFileChange(reader, lineMap);
+  }
+
+  void cleanup() noexcept {
+    restorePpCallbacks();
+
+    mReadersMapping.erase(mReader);
+    mReader = nullptr;
+    mReaderCallbacks = nullptr;
+  }
+
+  void restorePpCallbacks() noexcept
+  {
+    if(mReader == nullptr) {
+      return;
+    }
+    
+    if(mReader != parse_in) {
+      logError("fatal error: reader changed, abnormal termination...");
+      std::abort();
+    }
+
+    if(mReaderCallbacks != cpp_get_callbacks(mReader)) {
+      logError("fatal error: reader callbacks pointer changed, abnormal termination...");
+      std::abort();
+    }
+    
+    restoreCallback<&cpp_callbacks::file_change>();
+    restoreCallback<&cpp_callbacks::line_change>();
+    restoreCallback<&cpp_callbacks::used>();
+    restoreCallback<&cpp_callbacks::used_define>();
+  }
+
+  template <auto CallbackV>
+  void restoreCallback() noexcept
+  {
+    auto ppCallbacks = cpp_get_callbacks(mReader);
+
+    if(ppCallbacks->*CallbackV != mOurCallbacks.*CallbackV) {
+      logError("fatal error: reader callback was overwritten, abnormal termination...");
+      std::abort();
+    }
+    ppCallbacks->*CallbackV = mOriginalCallbacks.*CallbackV;
+  }
 
   //We do not need non-void callbacks for now and it would complicate code a fair bit,
   //so we do not support that case yet
@@ -241,90 +313,14 @@ private:
     }
   }
 
-  void handleLineChange(cpp_reader *reader, const cpp_token *token, int line)
+  template <auto HandlerV, auto OriginalHandlerV, typename... ArgsT>
+  void handleCallback(ArgsT... args)
   {
-    mMacroTracker.handleLineChange(reader, token, line);
+    (this->*HandlerV)(args...);
 
-    if(mOriginalCallbacks.line_change != nullptr) {
-      (mOriginalCallbacks.line_change)(reader, token, line);
-    }
-  }
-
-  void handleUsed(cpp_reader *reader, location_t loc, cpp_hashnode *node)
-  {
-    mMacroTracker.handleUsedKind(reader, loc, node);
-
-    if(mOriginalCallbacks.used != nullptr) {
-      (mOriginalCallbacks.used)(reader, loc, node);
-    }
-  }
-
-  void handleUsedDefine(cpp_reader *reader, location_t loc, cpp_hashnode *node)
-  {
-    mMacroTracker.handleUsedKind(reader, loc, node);
-
-    if(mOriginalCallbacks.used_define != nullptr) {
-      (mOriginalCallbacks.used_define)(reader, loc, node);
-    }
-  }
-
-  void cleanup() noexcept {
-    restorePpCallbacks();
-
-    mReadersMapping.erase(mReader);
-    mReader = nullptr;
-    mReaderCallbacksAddress = nullptr;
-  }
-
-  void restorePpCallbacks() noexcept
-  {
-    if(mReader == nullptr) {
-      return;
-    }
-    
-    if(mReader != parse_in) {
-      logError("fatal error: reader changed, abnormal termination...");
-      std::abort();
-    }
-
-    auto ppCallbacks = cpp_get_callbacks(mReader);
-    if(mReaderCallbacksAddress != ppCallbacks) {
-      logError("fatal error: reader callbacks pointer changed, abnormal termination...");
-      std::abort();
-    }
-
-    if(ppCallbacks->file_change != mOurCallbacks.file_change) {
-      logError("fatal error: reader callbacks were overriden, abnormal termination...");
-      std::abort();
-    }
-
-    if(ppCallbacks->used != mOurCallbacks.used) {
-      logError("fatal error: reader callbacks were overriden, abnormal termination...");
-      std::abort();
-    }
-
-    if(ppCallbacks->used_define != mOurCallbacks.used_define) {
-      logError("fatal error: reader callbacks were overriden, abnormal termination...");
-      std::abort();
-    }
-
-    if(ppCallbacks->line_change != mOurCallbacks.line_change) {
-      logError("fatal error: reader callbacks were overriden, abnormal termination...");
-      std::abort();
-    }
-
-    ppCallbacks->file_change = mOriginalCallbacks.file_change;
-    ppCallbacks->used = mOriginalCallbacks.used;
-    ppCallbacks->used_define = mOriginalCallbacks.used_define;
-    ppCallbacks->line_change = mOriginalCallbacks.line_change;
-  }
-
-  void handleFileChange(cpp_reader *reader, const line_map_ordinary *lineMap)
-  {
-    mHeadersTracker.handleFileChange(reader, lineMap);
-
-    if(mOriginalCallbacks.file_change != nullptr) {
-      (mOriginalCallbacks.file_change)(reader, lineMap);
+    auto oh = mOriginalCallbacks.*OriginalHandlerV;
+    if(oh != nullptr) {
+      oh(args...);
     }
   }
 };
